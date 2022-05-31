b5dd687c3c674a38b9ce16b6eb8180cc06452b67
INSTAGATOR <naiseips@pm.me>
2022-05-31 07:39:44 -0400

    Update navigation.jsx

--- src/App/Components/navigation.jsx
+++ src/App/Components/navigation.jsx
@@ -44,26 +44,6 @@ const history = useHistory()
               </a>
             </li>
             <li>
-              <a href='#services' className='page-scroll'>
-                Services
-              </a>
-            </li>
-            {/* <li>
-              <a href='#portfolio' className='page-scroll'>
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href='#testimonials' className='page-scroll'>
-                Testimonials
-              </a>
-            </li> */}
-            <li>
-              <a href='#team' className='page-scroll'>
-                Team
-              </a>
-            </li>
-            <li>
               <a href='#contact' className='page-scroll'>
                 Contact
               </a>
